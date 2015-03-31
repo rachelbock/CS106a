9a0a09c8712de47ac90d6acf60006e6c4b37ed6c
Samuel Bock <sbock@marinsoftware.com>
2015-03-30 19:19:22 -0700

    Updated Message

--- src/main/java/com/cs106a/TestClass.java
+++ src/main/java/com/cs106a/TestClass.java
@@ -11,6 +11,6 @@ import acm.program.ConsoleProgram;
 public class TestClass extends ConsoleProgram {
 
     public static void main (String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, universe!");
     }
 }
