1cad1dc1d95d5bc2dc7bed9e8501eecd0a746109
Samuel Bock <nonvirtualthunk@gmail.com>
2015-03-30 19:10:55 -0700

    Message!

---
+++ src/main/java/com/cs106a/TestClass.java
@@ -0,0 +1,16 @@
+package com.cs106a;
+
+import acm.program.ConsoleProgram;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nvt
+ * Date: 3/30/15
+ * Time: 6:58 PM
+ */
+public class TestClass extends ConsoleProgram {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, world");
+    }
+}
